915721329ab68f7dca80b684fece571c0eff1712
lcarlos <luiscardozoicg@gmail.com>
2023-07-09 19:14:45 -0500

    loaders and actions for edit clients

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import Layout from "./components/Layout";
 import NuevoCliente, {action as nuevoClienteAction} from "./pages/NuevoCliente";
 import Index, {loader as clientesLoader} from "./pages/Index";
+import EditarCliente,{loader as editarClienteLoader, action as editarClienteAction} from "./pages/EditarCliente";
+import ErrorPage from "./components/ErrorPage";
 
 
 const router = createBrowserRouter([
@@ -15,13 +17,22 @@ const router = createBrowserRouter([
       {
         index: true, 
         element: <Index/>,
-        loader: clientesLoader
+        loader: clientesLoader,
+        errorElement: <ErrorPage/>
       },
       {
       path: "/clientes/nuevo",
       element: <NuevoCliente/>,
-      action: nuevoClienteAction
-    }]
+      action: nuevoClienteAction,
+      errorElement: <ErrorPage/>
+    },
+    {
+    path: "/clientes/:clienteId/editar",
+    element: <EditarCliente/>,
+    loader: editarClienteLoader,
+    action: editarClienteAction,
+    errorElement: <ErrorPage/>
+  }]
     
   }
   
