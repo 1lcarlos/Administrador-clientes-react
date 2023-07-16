06ad254f370761c543c54645c62ed8bf474be5e4
lcarlos <luiscardozoicg@gmail.com>
2023-07-15 22:31:51 -0500

    create action eliminarCliente

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,4 +1,9 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Form, redirect } from "react-router-dom";
+import { eliminarCliente } from "../data/clientes";
+export async function action({ params }) {
+  await eliminarCliente(params.clienteId);
+  return redirect("/");
+}
 function Cliente({ cliente }) {
   const navigate = useNavigate();
   const { nombre, empresa, email, telefono, id } = cliente;
@@ -28,12 +33,22 @@ function Cliente({ cliente }) {
         >
           Editar
         </button>
-        <button
-          type="button"
-          className=" text-red-600 hover:text-red-700 uppercase font-bold text-xs"
+        <Form
+          method="post"
+          action={`/clientes/${id}/eliminar`}
+          onSubmit={(e) => {
+            if (!confirm("¿Deseas eliminar este registro?")) {
+              e.preventDefault();
+            }
+          }}
         >
-          Eliminar
-        </button>
+          <button
+            type="submit"
+            className=" text-red-600 hover:text-red-700 uppercase font-bold text-xs"
+          >
+            Eliminar
+          </button>
+        </Form>
       </td>
     </tr>
   );
